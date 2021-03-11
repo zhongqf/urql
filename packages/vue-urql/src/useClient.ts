4be8089455607ba2b3fb8498e3b44947a0113d9e
zhongqf <zhongqf@gmail.com>
2021-03-11 19:20:21 +0800

    make useClient can be used outside of setup()

--- packages/vue-urql/src/useClient.ts
+++ packages/vue-urql/src/useClient.ts
@@ -1,19 +1,25 @@
 import { App, inject, provide } from 'vue';
 import { Client, ClientOptions } from '@urql/core';
 
+type NullableClient = Client | undefined
+
+let outsideClient: NullableClient
+
 export function provideClient(opts: ClientOptions | Client) {
   const client = opts instanceof Client ? opts : new Client(opts);
+  outsideClient = client;
   provide('$urql', client);
   return client;
 }
 
 export function install(app: App, opts: ClientOptions | Client) {
   const client = opts instanceof Client ? opts : new Client(opts);
+  outsideClient = client;
   app.provide('$urql', client);
 }
 
 export function useClient(): Client {
-  const client = inject('$urql') as Client;
+  const client = inject('$urql') as Client || outsideClient;
   if (process.env.NODE_ENV !== 'production' && !client) {
     throw new Error(
       'No urql Client was provided. Did you forget to install the plugin or call `provideClient` in a parent?'
